a7d06b3a473857064b816eebb48cab8d8097fb0d
Siripuram <i27404@air-worldwide.com>
2022-09-16 15:50:32 +0530

    fixed the code for XMLHttpRequest

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const XMLHttpRequest = require('xhr2');
 try {
 
     // `who-to-greet` input defined in action metadata file
