95703900a818169e62c63a8f1028831fbf081405
Siripuram <i27404@air-worldwide.com>
2022-09-16 15:41:29 +0530

    built code using vercel/ncc

--- dist/index.js
+++ dist/index.js
@@ -9668,16 +9668,45 @@ const core = __nccwpck_require__(6452);
 const github = __nccwpck_require__(504);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+    const organizationId = core.getInput('organizationId');
+    const projectId = core.getInput('projectId');
+    const testplanid = core.getInput('testplanid');    
+    const testsuiteid = core.getInput('testsuiteid');
+    const patToken = core.getInput('PAT_TOKEN')
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', `https://dev.azure.com/${organizationId}/${projectId}/_apis/test/Plans/${testplanid}/Suites/${testsuiteid}/points?api-version=6.0`, true)
+    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8;');
+    request.setRequestHeader("Authorization", "Basic " + btoa('Basic' + ":" + patToken));
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+
+        var data = JSON.parse(this.response)
+
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data)
+        } else {
+            console.log('error')
+        }
+    }
+
+    request.send()
+
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
 })();
 
