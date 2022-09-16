8795be57bf9b2d385e72552d31d464b0963b4cd9
Siripuram <i27404@air-worldwide.com>
2022-09-16 15:38:44 +0530

    implementation to connect to azuredevops

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 
+    const organizationId = core.getInput('organizationId');
+    const projectId = core.getInput('projectId');
+    const testplanid = core.getInput('testplanid');    
+    const testsuiteid = core.getInput('testsuiteid');
+    const patToken = core.getInput('PAT_TOKEN')
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', `https://dev.azure.com/${organizationId}/${projectId}/_apis/test/Plans/${testplanid}/Suites/${testsuiteid}/points?api-version=6.0`, true)
+    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8;');
+    request.setRequestHeader("Authorization", "Basic " + btoa('Basic' + ":" + patToken));
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+
+        var data = JSON.parse(this.response)
+
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data)
+        } else {
+            console.log('error')
+        }
+    }
+
+    request.send()
+
 } catch (error) {
     core.setFailed(error.message);
 }
